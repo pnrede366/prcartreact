3bbc0036d896fb1a18c7eca3ecf21d13d48223d0
pnrede366 <pnrede366@gmail.com>
2022-01-10 19:43:46 +0530

    commit

--- Server.js
+++ Server.js
@@ -4,13 +4,17 @@ const cors = require("cors")
 const res = require("express/lib/response")
 const routes = require("./Routes/Server")
 const app = express()
+const PORT = process.env.PORT || 4000
 const url = "mongodb+srv://demo:demo@cluster0.xgbfl.mongodb.net/prcart?retryWrites=true&w=majority"
 
 app.use(express.json())
 app.use(cors())
-app.use(express.static("./Public/"))
+if (process.env.NODE_ENV=="production") {
+    app.use(express.static("./Public/"))
+    
+}
 mongoose.connect(url,()=>{console.log("db connected")})
-app.listen(4000,()=>{
+app.listen(PORT,()=>{
     console.log("server started at 4000");
 })
 
