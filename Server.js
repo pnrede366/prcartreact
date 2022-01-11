25ddae1ad86288c2533d9b6d2eeb865530b38ebf
pnrede366 <pnrede366@gmail.com>
2022-01-11 08:50:21 +0530

    updated

--- Server.js
+++ Server.js
@@ -9,10 +9,10 @@ const url = "mongodb+srv://demo:demo@cluster0.xgbfl.mongodb.net/prcart?retryWrit
 
 app.use(express.json())
 app.use(cors())
-if (process.env.NODE_ENV=="production") {
+
     app.use(express.static("./Public/"))
     
-}
+
 mongoose.connect(url,()=>{console.log("db connected")})
 app.listen(PORT,()=>{
     console.log("server started at 4000");
